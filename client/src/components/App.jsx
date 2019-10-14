ae8d11912558462c3317503688964b8dd5873839
Candy Wang <candywang013@gmail.com>
2019-10-14 15:44:06 -0700

    Implement and render react class component App

---
+++ client/src/components/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>testing from inside app.jsx</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
