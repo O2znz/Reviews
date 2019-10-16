0b4239cbf6bc941c1e62d0a7424a762fc78a1b19
Candy Wang <candywang013@gmail.com>
2019-10-16 05:10:06 -0700

    Add new state and pass state up from Search component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import Search from './Search.jsx';
 import RatingsList from './RatingsList.jsx';
 import ReviewsList from './ReviewsList.jsx';
 
@@ -7,10 +8,13 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      reviews: []
+      reviews: [],
+      filteredReviews: [],
+      search: ''
     };
 
     this.getAllReviews = this.getAllReviews.bind(this);
+    this.searchValue = this.searchValue.bind(this);
   }
 
   componentDidMount() {
@@ -22,10 +26,14 @@ class App extends React.Component {
       .then(result => this.setState({reviews: result.data}));
   }
 
+  searchValue(value) {
+    this.setState({search: value});
+  }
+
   render() {
-    // const ratings = this.state.reviews.map(review => review.ratings);
     return (
       <div>
+        <Search reviews={this.state.reviews} searchValue={this.searchValue} />
         <RatingsList reviews={this.state.reviews} />
         <ReviewsList reviews={this.state.reviews} />
       </div>
