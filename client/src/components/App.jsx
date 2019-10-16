03edb520c8ebadffff4493a8715d29cb1f832bcd
Candy Wang <candywang013@gmail.com>
2019-10-16 03:21:54 -0700

    Add RatingsList component and change state reviews value

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,17 +1,33 @@
 import React from 'react';
+import axios from 'axios';
+import RatingsList from './RatingsList.jsx';
+import ReviewsList from './ReviewsList.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      reviews: []
+    };
 
-    }
+    this.getAllReviews = this.getAllReviews.bind(this);
+  }
+
+  componentDidMount() {
+    this.getAllReviews();
+  }
+
+  getAllReviews() {
+    axios.get('/reviews')
+      .then(result => this.setState({reviews: result.data}))
   }
 
   render() {
+    // const ratings = this.state.reviews.map(review => review.ratings);
     return (
       <div>
-        <h1>testing from inside app.jsx</h1>
+        <RatingsList reviews={this.state.reviews} />
+        <ReviewsList reviews={this.state.reviews} />
       </div>
     );
   }
