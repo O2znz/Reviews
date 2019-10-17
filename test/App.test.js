1eeb9b6cf99433c035986bbba9ca3656af151b71
Candy Wang <candywang013@gmail.com>
2019-10-16 23:41:18 -0700

    Change test description

--- test/App.test.js
+++ test/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow, mount, render } from 'enzyme';
 import App from '../client/src/components/App.jsx';
 
-test('expects App to have state', () => {
+test('there are reviews, filtered reviews, and search', () => {
   const wrapper = shallow(<App />);
   expect(wrapper.state().reviews).toBeDefined();
   expect(wrapper.state().filteredReviews).toBeDefined();
