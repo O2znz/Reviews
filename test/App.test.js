3f49194c56053131dd86cb16334610db5a9c2709
Candy Wang <candywang013@gmail.com>
2019-10-16 23:36:54 -0700

    Change method to be consistent

--- test/App.test.js
+++ test/App.test.js
@@ -5,6 +5,6 @@ import App from '../client/src/components/App.jsx';
 test('expects App to have state', () => {
   const wrapper = shallow(<App />);
   expect(wrapper.state().reviews).toBeDefined();
-  expect(wrapper.state().filteredReviews).not.toBeUndefined();
-  expect(wrapper.state().search).not.toBeUndefined();
+  expect(wrapper.state().filteredReviews).toBeDefined();
+  expect(wrapper.state().search).toBeDefined();
 });
