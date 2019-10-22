3702d9193ce78098e9341d43969daa4127ff22ff
Candy Wang <candywang013@gmail.com>
2019-10-21 20:16:05 -0700

    Implement style changes

--- webpack.config.js
+++ webpack.config.js
@@ -15,9 +15,27 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
+          loader: 'babel-loader'
         }
-      } 
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              presets: [
+                '@babel/preset-env',
+                '@babel/preset-react'
+              ],
+              plugins: [
+                'babel-plugin-styled-components'
+              ]
+            }
+          }
+        ]
+      }
     ]
   }
 };
